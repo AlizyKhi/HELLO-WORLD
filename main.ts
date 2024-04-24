25675ef4e5dfea2d408b5dbc902defbf641e3c06
AlizyKhi <162714208+AlizyKhi@users.noreply.github.com>
2024-04-24 15:40:22 +0500

    HELLO-WORLD
    (assignment complete of IT Course

---
+++ main.ts
@@ -0,0 +1,8 @@
+let fullName:string = ("ALIZY,KHAN");
+console.log(fullName);
+let x:number =700
+let z:number = 700
+console.log(x+z)
+let statment = 'true';
+let statement = 'false';
+console .log(statement)
